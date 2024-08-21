fa7453a5fa59c89b4caa074f6f8346a6b6ad4b47
solomon.sitotaw <solomonsitotaw9@gmail.com>
2024-08-22 00:21:12 +0300

    Cleanup starter

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text className="bg-red-500">Home</Text>
+    </View>
+  );
+};
+
+export default Home;
