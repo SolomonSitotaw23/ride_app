23abe48d6c0ef2f7775db9a58ec0fff2ff1a2428
solomon.sitotaw <solomonsitotaw9@gmail.com>
2024-08-23 23:35:18 +0300

    Add signup functionality using clerk

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,20 +1,82 @@
-import { View, Text, ScrollView, Image } from 'react-native';
-import {} from 'react-native-safe-area-context';
-import { icons, images } from '@/constants';
-import InputField from '@/components/InputFIeld';
-import { useState } from 'react';
-import CustomButton from '@/components/CustomButton';
-import { Link } from 'expo-router';
-import OAuth from '@/components/OAuth';
+import { View, Text, ScrollView, Image, Alert } from "react-native";
+import {} from "react-native-safe-area-context";
+import { icons, images } from "@/constants";
+import InputField from "@/components/InputFIeld";
+import { useState } from "react";
+import CustomButton from "@/components/CustomButton";
+import { Link, router } from "expo-router";
+import OAuth from "@/components/OAuth";
+import { useSignUp } from "@clerk/clerk-expo";
+import ReactNativeModal from "react-native-modal";
 
 const SignUp = () => {
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
   const [form, setForm] = useState({
-    name: '',
-    email: '',
-    password: '',
+    name: "",
+    email: "",
+    password: "",
   });
 
-  const onSignUpPress = async () => {};
+  const [verification, setVerfication] = useState({
+    state: "default",
+    error: "",
+    code: "",
+  });
+
+  const onSignUpPress = async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      await signUp.create({
+        emailAddress: form.email,
+        password: form.password,
+      });
+
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      setVerfication({
+        ...verification,
+        state: "pending",
+      });
+    } catch (err: any) {
+      Alert.alert("Error", err.errors[0].longMessage);
+    }
+  };
+
+  const onPressVerify = async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code: verification.code,
+      });
+
+      if (completeSignUp.status === "complete") {
+        // Create a database user
+        await setActive({ session: completeSignUp.createdSessionId });
+        setVerfication({ ...verification, state: "success" });
+      } else {
+        setVerfication({
+          ...verification,
+          state: "failed",
+          error: "verification failed",
+        });
+        console.error(JSON.stringify(completeSignUp, null, 2));
+      }
+    } catch (err: any) {
+      setVerfication({
+        ...verification,
+        state: "failed",
+        error: err.errors[0].longMessage,
+      });
+    }
+  };
+
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white relative">
@@ -60,6 +122,57 @@ const SignUp = () => {
         </Link>
         {/* Verification Modal  */}
       </View>
+      <ReactNativeModal
+        isVisible={verification.state === "pending"}
+        onModalHide={() => {
+          if (verification.state === "success") setShowSuccessModal(true);
+        }}
+      >
+        <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+          <Text className="text-2xl font-JakartaExtraBold mb-2">
+            Verification
+          </Text>
+          <Text className=" font-Jakarta mb-5">
+            We've sent a verification code to {form.email}
+          </Text>
+          <InputField
+            label="Code"
+            icon={icons.lock}
+            placeholder="12345"
+            keyboardType="numeric"
+            onChangeText={(code) => setVerfication({ ...verification, code })}
+          />
+          {verification.error && (
+            <Text className="text-sm text-red-500 mt-1">
+              {verification.error}
+            </Text>
+          )}
+          <CustomButton
+            title="Verify Email"
+            onPress={onPressVerify}
+            className="mt-5 bg-success-500"
+          />
+        </View>
+      </ReactNativeModal>
+      <ReactNativeModal isVisible={showSuccessModal}>
+        <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+          <Image
+            source={images.check}
+            className="w-[110px] h-[110px] mx-auto my-1"
+          />
+          <Text className="text-3xl text-center font-Jakarta">Verified</Text>
+          <Text className="text-base text-gray-400 font-Jakarta text-center mt-2">
+            you have success fully verified your account
+          </Text>
+          <CustomButton
+            title="browse home"
+            onPress={() => {
+              router.replace("/(root)/(tabs)/home");
+            }}
+            className="mt-5"
+          />
+        </View>
+      </ReactNativeModal>
     </ScrollView>
   );
 };
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,52 +1,52 @@
-import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
-import { ButtonProps } from '@/types/type';
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+import { ButtonProps } from "@/types/type";
 
-const getBgVariantStyle = (variant: ButtonProps['bgVariant']) => {
+const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
   switch (variant) {
-    case 'secondary':
-      return 'bg-gray-500';
-    case 'danger':
-      return 'bg-red-500';
-    case 'success':
-      return 'bg-green-500';
-    case 'outline':
-      return 'bg-transparent border-neutral-300 border-[0.5px]';
+    case "secondary":
+      return "bg-gray-500";
+    case "danger":
+      return "bg-red-500";
+    case "success":
+      return "bg-green-500";
+    case "outline":
+      return "bg-transparent border-neutral-300 border-[0.5px]";
     default:
-      return 'bg-[#0286FF]';
+      return "bg-[#0286FF]";
   }
 };
 
-const getTextVariantStyle = (variant: ButtonProps['textVariant']) => {
+const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
   switch (variant) {
-    case 'primary':
-      return 'text-black';
-    case 'secondary':
-      return 'text-gray-100';
-    case 'danger':
-      return 'text-red-100';
-    case 'success':
-      return 'text-green-100';
+    case "primary":
+      return "text-black";
+    case "secondary":
+      return "text-gray-100";
+    case "danger":
+      return "text-red-100";
+    case "success":
+      return "text-green-100";
     default:
-      return 'text-white';
+      return "text-white";
   }
 };
 
 const CustomButton = ({
   onPress,
   title,
-  bgVariant = 'primary',
-  textVariant = 'default',
+  bgVariant = "primary",
+  textVariant = "default",
   IconLeft,
   IconRight,
-  className = '', // Ensure a default value
+  className = "", // Ensure a default value
   ...props
 }: ButtonProps) => {
   return (
     <TouchableOpacity
       onPress={onPress}
-      className={`${className} w-full rounded-full flex flex-row items-center justify-center shadow-md shadow-neutral-400/70 p-5 ${getBgVariantStyle(
-        bgVariant
+      className={`${className} w-full rounded-full flex flex-row items-center justify-center shadow-md shadow-neutral-400/70 p-4 ${getBgVariantStyle(
+        bgVariant,
       )}`}
       accessible={true}
       accessibilityLabel={title}
@@ -55,7 +55,7 @@ const CustomButton = ({
       {IconLeft && <IconLeft />}
       <Text
         className={`text-lg font-bold capitalize ${getTextVariantStyle(
-          textVariant
+          textVariant,
         )}`}
       >
         {title}
